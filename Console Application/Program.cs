0b0a27d2d7151dd118ed823d3edb045542215ef3
Hunter Haislip <hunlhais@ut.utm.edu>
2019-01-16 16:10:39 -0600

    Random Guessing Game.

--- Console Application/Program.cs
+++ Console Application/Program.cs
@@ -10,7 +10,35 @@ namespace Console_Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to the guessing game!");
+            Console.WriteLine("Please pick a number between 0 and 100");
+            Random Rand = new Random();
+            int Number = Rand.Next(101);
+
+            int In;
+            int Count = 0;
+            In = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(In);
+            while (Number != In)
+            {
+
+                if (In < Number)
+                {
+                    Console.WriteLine("To Low");
+                }
+
+                else
+                {
+                    Console.WriteLine("To High");
+                }
+
+                Console.WriteLine("Please pick a number between 0 and 100");
+                In = Convert.ToInt32(Console.ReadLine());
+                Count++;
+
+            }
+            Console.WriteLine("You Won!");
+            Console.WriteLine("You took " + Count + " tries");
         }
     }
 }
